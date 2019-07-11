ba9d6625c6736504c2167e5519b2fc8962ac419a
Martin <martin.rishovd@apx-systems.com>
2019-07-11 14:50:09 +0200

    fdsfds

---
+++ WarriorWars/WarriorWars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class EntryPoint
+    {
+        static Random rng = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Jim", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rng.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+
+
+            }
+
+            goodGuy.Attack(badGuy);
+        }
+    }
+}
---
+++ WarriorWars/WarriorWars/Warrior.cs
@@ -0,0 +1,77 @@
+﻿/*sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System;
+using System.Threading;
+using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+
+        private readonly Faction FACTION;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+        }
+
+        private Weapon weapon;
+        private Armor armor;
+
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            FACTION = faction;
+            isAlive = true;
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = BAD_GUY_STARTING_HEALTH;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+            enemy.health -= damage;
+
+            if (enemy.health <= 0)
+            {
+                enemy.isAlive = false;
+                Console.WriteLine($"{enemy.name} is dead! {name} is victorious!");
+
+            }
+            else
+            {
+                Console.WriteLine($"{name} attacked {enemy.name}. {damage} damage was infliced to {enemy.name}, remaning health is {enemy.health} of {enemy.name}");
+            }
+            Thread.Sleep(200);
+
+        }
+    }
+}
